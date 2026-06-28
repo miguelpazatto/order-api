d332d45d4335eedc6098224433bbaf129844ca83
miguelpazatto <miguelpazatto60@gmail.com>
2026-06-27 21:31:24 -0300

    feat: create domain entities and flyway migration schema

---
+++ src/main/java/com/miguelpazatto/orderapi/entities/Customer.java
@@ -0,0 +1,31 @@
+package com.miguelpazatto.orderapi.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+
+}
